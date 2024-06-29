2182fef5cf3547897209ca6df8d14983092cd837
Aldino Efendi <aldino.efendi@icloud.com>
2024-06-30 04:10:52 +0700

    feat: add share link

--- iLeague/Matches/MatchesView.swift
+++ iLeague/Matches/MatchesView.swift
@@ -12,12 +12,14 @@ struct MatchesView: View {
     
     @Environment(\.modelContext) var modelContext
     @Query private var matches: [MatchModel]
+    @State private var listDates: [Date] = []
+    @State private var filteredMatches: [MatchModel] = []
     @State private var isPresented: Bool = false
     @State private var count: Int = 0
     
     var body: some View {
         List {
-            ForEach(Array(matches.enumerated()), id: \.offset) { index, match in
+            ForEach(Array(filteredMatches.enumerated()), id: \.offset) { index, match in
                 Section {
                     VStack(alignment: .leading) {
                         HStack {
@@ -38,13 +40,29 @@ struct MatchesView: View {
             AddMatchView()
         })
         .toolbar {
-            ToolbarItem {
-                Button("Add") {
-                    isPresented.toggle()
+            ToolbarItem(placement: .topBarLeading) {
+                ShareLink(item: generateCSV(filteredMatches))
+            }
+            ToolbarItem(placement: .topBarTrailing) {
+                HStack {
+                    Menu {
+                        ForEach(listDates, id: \.self) { date in
+                            Button(date.convertDateWithFormat()) {
+                                selectDateMatch(date: date)
+                            }
+                        }
+                    } label: {
+                        Image(systemName: "line.3.horizontal.decrease.circle")
+                    }
+                    
+                    Button("Add") {
+                        isPresented.toggle()
+                    }
                 }
             }
         }
-        .navigationTitle("Match List")
+        .navigationTitle(filteredMatches.first?.date.convertDateWithFormat() ?? "Match List")
+        .onAppear { fetchMatches() }
     }
     
     func deleteMatch(_ indexSet: IndexSet) {
@@ -56,6 +74,37 @@ struct MatchesView: View {
 }
 
 extension MatchesView {
+    
+    func selectDateMatch(date: Date) {
+        filteredMatches = matches.filter { $0.date == date }
+    }
+    
+    func fetchMatches() {
+        listDates = matches.uniques(by: \.date).map { $0.date }
+        filteredMatches = matches.filter { $0.date == matches.first?.date }
+    }
+    
+    func generateCSV(_ data: [MatchModel]) -> URL {
+        let todayDate = Date().convertDateWithFormat()
+        var fileURL: URL!
+        let heading = "levelMatch,player1,player2,player3,player4,score\n"
+        let rows = data.map {
+            "\($0.player1.level)\($0.player2.level) - \($0.player3.level)\($0.player4.level)," +
+            "\($0.player1.name),\($0.player2.name),\($0.player3.name),\($0.player4.name)," +
+            "\($0.scoreA) - \($0.scoreB)"
+        }
+        let stringData = heading + rows.joined(separator: "\n")
+        
+        do {
+            let path = try FileManager.default.url(for: .documentDirectory, in: .allDomainsMask, appropriateFor: nil, create: false)
+            fileURL = path.appending(path: "Match Data - \(todayDate).csv", directoryHint: .checkFileSystem)
+            try stringData.write(to: fileURL, atomically: true, encoding: .utf8)
+        } catch {
+            print("Error generate csv file")
+        }
+        return fileURL
+    }
+    
     func matchView(_ match: MatchModel) -> some View {
         VStack(alignment: .leading) {
             HStack {
